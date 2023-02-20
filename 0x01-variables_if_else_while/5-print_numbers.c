215637fc26f24c54ee468882f115cfade738c1ff
TerrenceA1 <terrencekibue@gmail.com>
2023-02-20 11:24:23 -0800

    printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - a program that prints single numbers of base 10
+ * return :(0) success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
